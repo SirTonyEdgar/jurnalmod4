e1fe8d16cb9e9c7647adc413ad44fa13bb2de5a8
SirTonyEdgar <daffamuhammad625@gmail.com>
2024-03-16 09:10:55 +0700

    tes

--- jurnalmod4/Program.cs
+++ jurnalmod4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah kodebuah = new KodeBuah();
+
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Ceri));
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Apel));
+            Console.WriteLine(kodebuah.getKodeBuah(KodeBuah.daftarBuah.Melon));
+        }
+    }
+}
